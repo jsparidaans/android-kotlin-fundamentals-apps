bf1e580a1e05ac0ac4804cb309040f451bc08768
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-10 10:23:10 +0100

    added viewmodel databinding to GameFragment.kt

--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -54,6 +54,9 @@ class GameFragment : Fragment() {
         // Get the viewModel
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
+        // Set up viewModel data binding
+        binding.gameViewModel = viewModel
+
         // Set up ViewModel Observers
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
@@ -67,24 +70,7 @@ class GameFragment : Fragment() {
             }
         })
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        binding.endGameButton.setOnClickListener { onEndGame() }
         return binding.root
-    }
-
-    /** Methods for buttons presses **/
-
-    private fun onSkip() {
-        viewModel.onSkip()
-    }
-
-    private fun onCorrect() {
-        viewModel.onCorrect()
-    }
-
-    private fun onEndGame() {
-        gameFinished()
     }
 
     /**
