2122c9ac1a78192e13427b81bb4357d61ac44db9
Joey Sparidaans <joey.sparidaans@myidbv.com>
2020-12-10 10:30:51 +0100

    formatting

--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -19,7 +19,7 @@ package com.example.android.guesstheword.screens.score
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ScoreViewModelFactory (private val finalScore: Int) : ViewModelProvider.Factory {
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
